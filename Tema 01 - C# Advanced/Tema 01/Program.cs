91e2b832a10357ee48605b158bc302fa5f1691d2
DESKTOP-3D2OJRN\Amalia <amaliagr24@yahoo.com>
2022-03-09 23:43:06 +0200

    Added Tema 01 - C# Advanced

---
+++ Tema 01 - C# Advanced/Tema 01/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+
+namespace Tema_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Set<int> mySet = new Set<int>();
+            Set<int> merged = new Set<int>();
+            Set<int> filtered = new Set<int>();
+
+            try
+            {
+                mySet.Insert(1);
+                mySet.Insert(2);
+                mySet.Insert(3);
+                mySet.Insert(4);
+                mySet.Insert(5);
+                mySet.Insert(3);
+              
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + '\n');
+            }
+            
+            Console.WriteLine("After creating the set: ");
+            mySet.PrintItems();
+
+            try
+            {
+                mySet.Remove(1);
+                mySet.Remove(6);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + '\n');
+            }
+          
+            Console.WriteLine("After removing: ");
+            mySet.PrintItems();
+            
+
+            if (mySet.Contains(1))
+            {
+                Console.WriteLine("Yes, the list contains 1.");
+            } else
+            {
+                Console.WriteLine("No, the list does not contain 1. ");
+            }
+
+            try
+            {
+                Set<int> anotherSet = new Set<int>();
+                anotherSet.Insert(7);
+                anotherSet.Insert(2);
+                anotherSet.Insert(8);
+
+                merged = mySet.Merge(anotherSet);
+                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            Console.WriteLine("Merged list is: ");
+            merged.PrintItems();
+            
+
+
+            filtered = mySet.Filter(x => x > 3);
+            Console.WriteLine("Filtered list is: ");
+            filtered.PrintItems();
+
+
+        }
+    }
+
+}
+
+
---
+++ Tema 01 - C# Advanced/Tema 01/Set.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema_01
+{
+    class Set<T> : IEnumerable<T>
+    {
+        public List<T> List;
+
+        public Set()
+        {
+            this.List = new List<T>();
+        }
+
+        /*
+        * Inserts an item in the list.
+        */
+        public void Insert(T item)
+        {
+            if (List.Contains(item))
+            {
+                throw new Exception("Set can not contain duplicate item.");
+            } else
+            {
+                List.Add(item);
+            }
+        }
+
+        /*
+        * Removes an item from the list.
+        */
+        public void Remove(T item)
+        {
+            if (!List.Contains(item)) {
+                throw new Exception("Can not remove non-existing items.");
+            } else
+            {
+                List.Remove(item);
+            }
+            
+        }
+
+        /*
+        * Checks if the item is already in the list.
+        */
+        public bool Contains(T item) 
+        {
+            if (List.Contains(item))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /*
+         * Returns an object of the class Set, containing the concatenation 
+         * of two lists.
+         */
+        public Set<T> Merge(Set<T> other)
+        {
+            List<T> mergedList = new List<T>();
+            mergedList.AddRange(List);
+            
+            foreach (T item in other)
+            {
+                mergedList.Add(item);
+            }
+
+            Set<T> merged = new Set<T>();
+            foreach (T item in mergedList)
+            {
+                try
+                {
+                    merged.Insert(item);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Duplicates not allowed");
+                }
+            }
+
+            return merged;
+
+
+        }
+
+        /*
+        * Returns an object of the class Set, containing only items 
+        * that meet a condition.
+        */
+        public Set<T> Filter(Func<T, bool> lambdaFunction)
+        {
+            Set<T> subset = new Set<T>();
+            var filteredList = List.Where(lambdaFunction).ToList();
+
+            foreach (T item in filteredList)
+            {
+               try
+               {
+                    subset.Insert(item);
+               }
+               catch(Exception)
+               {
+                    Console.WriteLine("Duplicates not allowed.");
+               }
+            }
+
+            return subset;
+
+        }
+
+        /*
+        * Method used to print the items in the current set.
+        */
+        public void PrintItems()
+        {
+            foreach (T item in List)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return List.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+      
+    }
+}
