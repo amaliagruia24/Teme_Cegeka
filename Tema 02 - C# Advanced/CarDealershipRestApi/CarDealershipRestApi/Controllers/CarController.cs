421aa6e08ab1bda3578e515b39baf1c6d4cee037
DESKTOP-3D2OJRN\Amalia <amaliagr24@yahoo.com>
2022-03-14 20:32:07 +0200

    Added Tema 02 - C# Advanced

---
+++ Tema 02 - C# Advanced/CarDealershipRestApi/CarDealershipRestApi/Controllers/CarController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CarDealershipRestApi.Models;
+using CarDealershipRestApi.Repositories;
+
+namespace CarDealershipRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        ICarsRepository carsRepository;
+        
+        public CarController(ICarsRepository carsRepository)
+        {
+            this.carsRepository = carsRepository;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<List<Car>>> Get()
+        {
+            return Ok(carsRepository.GetCarsList());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Car>> Get(int id)
+        {
+            var car = this.carsRepository.GetCarsList().Find(c => c.Id == id);
+            if (car == null)
+            {
+                return BadRequest("Car not found.");
+            }
+            return Ok(car);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Car>>> AddCar(Car car)
+        {
+            this.carsRepository.GetCarsList().Add(car);
+            return Ok(this.carsRepository.GetCarsList());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Car>>> UpdateCar(Car request)
+        {
+            var car = this.carsRepository.GetCarsList().Find(c => c.Id == request.Id);
+            if (car == null)
+            {
+                return BadRequest("Car not found.");
+            }
+
+            car.CarModel = request.CarModel;
+            car.Color = request.Color;
+            car.Year = request.Year;
+            car.Fuel = request.Fuel;
+
+            return Ok(this.carsRepository.GetCarsList());
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Car>>> Delete(int id)
+        {
+            var car = this.carsRepository.GetCarsList().Find(c => c.Id == id);
+            if (car == null)
+            {
+                return BadRequest("Car not found.");
+            }
+            this.carsRepository.GetCarsList().Remove(car);
+            return Ok(carsRepository.GetCarsList());
+        }
+
+
+    }
+}
---
+++ Tema 02 - C# Advanced/CarDealershipRestApi/CarDealershipRestApi/Repositories/CustomersRepository.cs
@@ -0,0 +1,38 @@
+﻿
+using CarDealershipRestApi.Models;
+namespace CarDealershipRestApi.Repositories
+{
+    public class CustomersRepository : ICustomersRepository
+    {
+        List<Customer> customers = new List<Customer>
+        {
+            new Customer
+            {
+                CustomerId = 1,
+                CustomerName = "Dragos Anca",
+                BoughtCars = new List<Car>
+                {
+                    new Car
+                    {
+                        Id = 1,
+                        CarModel = "Toyota",
+                        Color = "Red",
+                        Year = 2010,
+                        Fuel = "Diesel"
+                    }
+                }
+            },
+            new Customer
+            {
+                CustomerId = 2,
+                CustomerName = "Amalia Gruia",
+                BoughtCars = new List<Car>()
+            }
+        };
+
+        public List<Customer> GetCustomers()
+        {
+            return customers;
+        }
+    }
+}
