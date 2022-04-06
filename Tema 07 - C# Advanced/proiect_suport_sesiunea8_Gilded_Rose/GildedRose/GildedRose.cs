0758d0649bcc9c816a08acbd91b2cf63207cfbea
DESKTOP-3D2OJRN\Amalia <amaliagr24@yahoo.com>
2022-04-07 00:07:13 +0300

    Added Tema 07 - SOLID

---
+++ Tema 07 - C# Advanced/proiect_suport_sesiunea8_Gilded_Rose/GildedRose/GildedRose.cs
@@ -0,0 +1,52 @@
+﻿using GildedRose;
+using System.Collections.Generic;
+
+namespace GildedRoseKata
+{
+    public class GildedRose
+    {
+        private const string AgedBrie = "Aged Brie";
+        private const string Backstage = "Backstage passes to a TAFKAL80ETC concert";
+        private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+        private const string Conjured = "Conjured";
+
+        IList<Item> Items;
+        public GildedRose(IList<Item> Items)
+        {
+            //aplicati Factory Pattern on init
+            IList<Item> factoryItems = new List<Item>();
+            foreach (Item item in Items)
+            {
+                switch(item.Name)
+                {
+                    case AgedBrie:
+                        factoryItems.Add(ItemFactory.createItem(
+                            ItemFactory.ItemType.AgedBrie, item.Name, item.SellIn, item.Quality));
+                        break;
+                    case Backstage:
+                        factoryItems.Add(ItemFactory.createItem(
+                            ItemFactory.ItemType.BackStage, item.Name, item.SellIn, item.Quality));
+                        break;
+                    case Conjured:
+                        factoryItems.Add(ItemFactory.createItem(
+                            ItemFactory.ItemType.Conjured, item.Name, item.SellIn, item.Quality));
+                        break;
+                    case Sulfuras:
+                        factoryItems.Add(ItemFactory.createItem(
+                            ItemFactory.ItemType.Sulfuras, item.Name, item.SellIn, item.Quality));
+                        break;
+                }
+            }
+            this.Items = factoryItems;
+        }
+
+        public void UpdateQuality()
+        {
+            foreach (var item in Items)
+            {
+                item.ChangeDay();
+                item.updateQuality();
+            }
+        }
+    }
+}
---
+++ Tema 07 - C# Advanced/proiect_suport_sesiunea8_Gilded_Rose/GildedRose/ItemFactory.cs
@@ -0,0 +1,32 @@
+﻿using GildedRoseKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRose
+{
+    public class ItemFactory
+    {
+        public enum ItemType
+        {
+            AgedBrie, BackStage, Sulfuras, Conjured
+        }
+
+        public static Item createItem(ItemType itemType, string name, int sellIn, int quality)
+        {
+            switch (itemType)
+            {
+                case ItemType.AgedBrie: return new AgeBrie(name, sellIn, quality);
+                case ItemType.Sulfuras: return new Sulfuras(name, sellIn, quality);
+                case ItemType.BackStage: return new BackStage(name, sellIn, quality);
+                case ItemType.Conjured: return new Conjured(name, sellIn, quality);
+
+            }
+            throw new ArgumentException($"The item type {itemType} is not recognized.");
+        }
+    }
+
+}
+
